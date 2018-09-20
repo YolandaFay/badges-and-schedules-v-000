c42b8fee22ac5c9461635d3960bbbf34e14f237f
Yolanda Fay <yolandafay@gmail.com>
2018-09-20 02:42:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,15 +6,10 @@ def batch_badge_creator(name_array)
   name_array.collect{|name| "Hello, my name is #{name}."}
 end
 
-#def assign_rooms(speaker_array)
- # counter = 0
-  #speaker_array.each do |speaker| 
-   # speaker_array[counter] = "Hello, #{speaker}! You'll be assigned to room #{counter+1}!"
-   # counter += 1 
-  #end
-#end
-
 def assign_rooms(speaker_array)
   counter = 0
   speaker_array.collect{|speaker| "Hello, #{speaker}! You'll be assigned to room #{counter+=1}!"}
 end
+
+def printer()
+  
